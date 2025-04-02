7cd7ac5661040127dd5c25538696449cb796e394
Dapringer <allerlei-03@web.de>
2025-04-02 10:49:05 +0200

    Added second UltraSonic Sensor and refactored Distance Detection

--- UltraSonicAndLED.ino
+++ UltraSonicAndLED.ino
@@ -1,14 +1,14 @@
-#define PIN_TRIGGER 12
-#define PIN_ECHO 11
-#define LED_PIN 13
+#define PIN_TRIGGER_F 12
+#define PIN_ECHO_F 13
+#define PIN_TRIGGER_B 10
+#define PIN_ECHO_B 11
 
-#define EnA 10
-#define EnB 5
-#define In1 9
-#define In2 8
-#define In3 7
+#define EnA 2
+#define In1 3
+#define In2 4
+#define In3 5
 #define In4 6
-
+#define EnB 7
 
 
 const int SENSOR_MAX_RANGE = 300;  // in cm
@@ -17,25 +17,33 @@ unsigned int distance;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PIN_TRIGGER, OUTPUT);
-  pinMode(PIN_ECHO, INPUT);
-  pinMode(LED_PIN, OUTPUT);
+
+  pinMode(PIN_TRIGGER_F, OUTPUT);
+  pinMode(PIN_ECHO_F, INPUT);
+  // pinMode(PIN_TRIGGER_R, OUTPUT);
+  // pinMode(PIN_ECHO_R, INPUT);
+  pinMode(PIN_TRIGGER_B, OUTPUT);
+  pinMode(PIN_ECHO_B, INPUT);
+  // pinMode(PIN_TRIGGER_L, OUTPUT);
+  // pinMode(PIN_ECHO_L, INPUT);
 
 
   pinMode(EnA, OUTPUT);
-  pinMode(EnB, OUTPUT);
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
-
+  pinMode(EnB, OUTPUT);
 }
 
 void loop() {
-  int distance = detectDistance();
 
-  if (distance > 50 && distance < SENSOR_MAX_RANGE) {
-    digitalWrite(LED_PIN, LOW);
+  int distance_front = readUltraSonic(1);
+  // int distance_right = readUltraSonic(2);
+  int distance_back = readUltraSonic(3);
+  // int distance_left = readUltraSonic(4);
+
+  if (!isTooNear(distance_front) && !isTooNear(distance_back)) {
     driveMotors(255, 1);
     driveMotors(255, 2);
   } else {
@@ -45,43 +53,92 @@ void loop() {
   delay(100);
 }
 
-void emergencyBreak() {
-  Serial.print("Emergency Stop!");
-  digitalWrite(LED_PIN, HIGH);
+bool isTooNear(int distance) {
+  if (distance > 5) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
+void emergencyBreak() {
   digitalWrite(In1, LOW);
   digitalWrite(In2, LOW);
   digitalWrite(In3, LOW);
   digitalWrite(In4, LOW);
-}
 
-
-int detectDistance() {
-  digitalWrite(PIN_TRIGGER, LOW);
-  delayMicroseconds(2);
-
-  digitalWrite(PIN_TRIGGER, HIGH);
-  delayMicroseconds(10);
-
-  duration = pulseIn(PIN_ECHO, HIGH);
-  distance = duration / 58;
-  
-  Serial.println("Distance to object: " + String(distance) + " cm");
-  return distance;
+  Serial.println("Emergency Stop!");
 }
 
 
 void driveMotors(int motorSpeed, int motorID) {
+  Serial.print("Motor Speed: ");
+  Serial.print(motorSpeed);
+  Serial.print(" | Motor ID: ");
+  Serial.println(motorID);
   switch (motorID) {
     case 1:
       digitalWrite(In1, HIGH);
-        digitalWrite(In2, LOW);
+      digitalWrite(In2, LOW);
       analogWrite(EnA, motorSpeed);
+      break;
     case 2:
       digitalWrite(In3, HIGH);
-        digitalWrite(In4, LOW);
+      digitalWrite(In4, LOW);
       analogWrite(EnB, motorSpeed);
+      break;
     default:
       return;
   }
+}
+
+
+int readUltraSonic(int sensorID) {
+  switch (sensorID) {
+    case 1:
+      digitalWrite(PIN_TRIGGER_F, LOW);
+      delayMicroseconds(2);
+
+      digitalWrite(PIN_TRIGGER_F, HIGH);
+      delayMicroseconds(10);
+
+      duration = pulseIn(PIN_ECHO_F, HIGH);
+      break;
+      // case 2:
+      //   digitalWrite(PIN_TRIGGER_R, LOW);
+      //   delayMicroseconds(2);
+
+      //   digitalWrite(PIN_TRIGGER_R, HIGH);
+      //   delayMicroseconds(10);
+
+      //   duration = pulseIn(PIN_ECHO_R, HIGH);
+      break;
+    case 3:
+      digitalWrite(PIN_TRIGGER_B, LOW);
+      delayMicroseconds(2);
+
+      digitalWrite(PIN_TRIGGER_B, HIGH);
+      delayMicroseconds(10);
+
+      duration = pulseIn(PIN_ECHO_B, HIGH);
+      // case 4:
+      //     digitalWrite(PIN_TRIGGER_L, LOW);
+      //   delayMicroseconds(2);
+
+      //   digitalWrite(PIN_TRIGGER_L, HIGH);
+      //   delayMicroseconds(10);
+
+      //   duration = pulseIn(PIN_ECHO_L, HIGH);
+      break;
+    default:
+      break;
+  }
+
+  distance = (duration / 58);
+  Serial.print("Distance to object from Sensor ");
+  Serial.print(sensorID);
+  Serial.print(". Measured Distance: ");
+  Serial.println(distance);
+
+  return distance;
 }

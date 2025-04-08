a9785de96b7cad75f0f54efc45a440f999b0dcce
Dapringer <allerlei-03@web.de>
2025-04-08 11:31:46 +0200

    updated senser enumerators and changed inputs frin pwn to analog

--- 250404-105518-megaatmega2560/src/UltraSonicAndLED.ino
+++ 250404-105518-megaatmega2560/src/UltraSonicAndLED.ino
@@ -5,10 +5,15 @@
 #include "class/Motor.h"
 #include "class/Sensor.h"
 
+#define SENSOR_TRGGER 21
 #define PIN_TRIGGER_F 12
-#define PIN_ECHO_F 13
+#define PIN_ECHO_F A1
+#define PIN_TRIGGER_R 21
+#define PIN_ECHO_R A2
 #define PIN_TRIGGER_B 10
-#define PIN_ECHO_B 11
+#define PIN_ECHO_B A3
+#define PIN_TRIGGER_L 10
+#define PIN_ECHO_L A8
 
 #define EnA 2
 #define In1 3
@@ -27,8 +32,10 @@ unsigned int distance;
 Motor motor1(1, In1, In2, EnA);
 Motor motor2(2, In3, In4, EnB);
 
-Sensor sensor1(1, PIN_TRIGGER_F, PIN_ECHO_F);
-Sensor sensor2(2, PIN_TRIGGER_B, PIN_ECHO_B);
+Sensor sensor1(1, SENSOR_TRGGER, PIN_ECHO_F);
+Sensor sensor2(2, SENSOR_TRGGER, PIN_ECHO_R);
+Sensor sensor3(3, SENSOR_TRGGER, PIN_ECHO_B);
+Sensor sensor4(4, SENSOR_TRGGER, PIN_ECHO_L);
 
 void setup()
 {
@@ -38,6 +45,12 @@ void setup()
 void loop()
 {
   sensor1.printStatus();
+  sensor2.printStatus();
+  sensor3.printStatus();
+  sensor4.printStatus();
 
+  motor1.drive(97, 1);
+  motor2.drive(100, 1);
+  Serial.println("------------------");
   delay(100);
 }

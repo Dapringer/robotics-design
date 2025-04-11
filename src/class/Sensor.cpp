0eac62d0eae325aed858cb4c7f2640535675cd4e
Dapringer <allerlei-03@web.de>
2025-04-11 13:15:09 +0200

    refactor Sensor class to support interrupt-based operation and improve sensor registration

--- src/class/Sensor.cpp
+++ src/class/Sensor.cpp
@@ -3,18 +3,114 @@
 
 #include "Sensor.h"
 
+Sensor *Sensor::sensors_[4] = {nullptr, nullptr, nullptr, nullptr}; // Initialize the static array
+
+volatile unsigned long echoStart = 0;
+volatile unsigned long echoEnd = 0;
+volatile bool echoDone = false;
+
 // Constructor
-Sensor::Sensor(int id, int triggerPin, int echoPin)
-    : id_(id), triggerPin_(triggerPin), echoPin_(echoPin), duration_(0), distance_(0), measurementIndex_(0)
+Sensor::Sensor(int id, int triggerPin, int echoPin, bool useInterrupt)
+    : id_(id), triggerPin_(triggerPin), echoPin_(echoPin), useInterrupt_(useInterrupt), duration_(0), distance_(0), measurementIndex_(0)
 {
     pinMode(triggerPin_, OUTPUT);
     pinMode(echoPin_, INPUT);
 
-    // Initialize the measurement array
     for (int i = 0; i < 5; i++)
     {
         measurements_[i] = 0;
     }
+
+    if (useInterrupt_)
+    {
+        registerSensor(this); // Register this sensor for interrupts
+        switch (id_)
+        {
+        case 0:
+            attachInterrupt(digitalPinToInterrupt(echoPin_), echoISR0, CHANGE);
+            break;
+        case 1:
+            attachInterrupt(digitalPinToInterrupt(echoPin_), echoISR1, CHANGE);
+            break;
+        case 2:
+            attachInterrupt(digitalPinToInterrupt(echoPin_), echoISR2, CHANGE);
+            break;
+        case 3:
+            attachInterrupt(digitalPinToInterrupt(echoPin_), echoISR3, CHANGE);
+            break;
+        }
+    }
+}
+
+// Register a sensor
+void Sensor::registerSensor(Sensor *sensor)
+{
+    if (sensor->id_ >= 0 && sensor->id_ < 4)
+    {
+        sensors_[sensor->id_] = sensor;
+    }
+    else
+    {
+        Serial.println("Error: Sensor ID out of range!");
+    }
+}
+
+// Handle interrupt for a specific sensor
+void Sensor::handleInterrupt(int sensorId)
+{
+    if (sensors_[sensorId] != nullptr)
+    {
+        Sensor *sensor = sensors_[sensorId];
+        if (digitalRead(sensor->getEchoPin()) == HIGH)
+        {
+            sensor->duration_ = micros();
+            // Serial.print("Sensor ");
+            // Serial.print(sensorId);
+            // Serial.println(": Echo start detected");
+        }
+        else
+        {
+            sensor->distance_ = (micros() - sensor->duration_) / 58.0;
+            // Serial.print("Sensor ");
+            // Serial.print(sensorId);
+            // Serial.print(": Distance calculated: ");
+            // Serial.println(sensor->distance_);
+        }
+    }
+}
+
+// ISR for sensor 0
+void Sensor::echoISR0()
+{
+    handleInterrupt(0);
+}
+
+// ISR for sensor 1
+void Sensor::echoISR1()
+{
+    handleInterrupt(1);
+}
+
+// ISR for sensor 2
+void Sensor::echoISR2()
+{
+    handleInterrupt(2);
+}
+
+// ISR for sensor 3
+void Sensor::echoISR3()
+{
+    handleInterrupt(3);
+}
+
+// Trigger method for interrupt-based sensors
+void Sensor::trigger()
+{
+    digitalWrite(triggerPin_, LOW);
+    delayMicroseconds(2);
+    digitalWrite(triggerPin_, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin_, LOW);
 }
 
 bool Sensor::isWallApproaching()
@@ -92,20 +188,33 @@ bool Sensor::isWallApproaching()
 // Method to read distance
 void Sensor::readDistance()
 {
-    digitalWrite(triggerPin_, LOW);
-    delayMicroseconds(2);
+    if (useInterrupt_)
+    {
+        trigger();
+    }
+    else
+    {
+        digitalWrite(triggerPin_, LOW);
+        delayMicroseconds(2);
 
-    digitalWrite(triggerPin_, HIGH);
-    delayMicroseconds(10);
+        digitalWrite(triggerPin_, HIGH);
+        delayMicroseconds(10);
 
-    digitalWrite(triggerPin_, LOW);
-    duration_ = pulseIn(echoPin_, HIGH);
-    distance_ = duration_ / 58.0; // Convert duration to distance in cm
+        digitalWrite(triggerPin_, LOW);
+        duration_ = pulseIn(echoPin_, HIGH);
+        distance_ = duration_ / 58.0; // Convert duration to distance in cm
+    }
 }
 
 unsigned int Sensor::getDistance() const
 {
     return distance_;
+}
+
+// Getter for echoPin_
+int Sensor::getEchoPin() const
+{
+    return echoPin_;
 }
 
 // Method to print sensor status

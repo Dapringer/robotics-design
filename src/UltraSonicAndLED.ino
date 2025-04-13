fa57bee7e31098eeb081bbd4e74dd051b7d8b8a9
fred321f <frederick01nielsen@gmail.com>
2025-04-13 17:53:10 +0200

    pseudo code added

--- src/UltraSonicAndLED.ino
+++ src/UltraSonicAndLED.ino
@@ -1,6 +1,6 @@
-#include <string.h>
 #include <stdlib.h>
 #include <Arduino.h>
+#include <Wire.h>
 
 #include "class/Motor.h"
 #include "class/Sensor.h"
@@ -22,11 +22,14 @@
 #define In4 12
 #define EnB 13
 
+#define I2C_SDA 20
+#define I2C_SCL 21
+#define I2C_ADDRESS 0x08
+#define I2C_INTERRUPT_PIN 2
+
 #define MAX_SPEED 150
 #define MIN_SPEED 75
 #define EMERGENCY_STOP_DISTANCE 5
-
-using namespace std;
 
 const int SENSOR_MAX_RANGE = 300; // in cm
 const int MIN_DIST = 15;
@@ -41,9 +44,35 @@ Sensor sensorRight(2, SENSOR_TRGGER, PIN_ECHO_R, true);
 Sensor sensorBack(3, SENSOR_TRGGER, PIN_ECHO_B, true);
 Sensor sensorLeft(4, SENSOR_TRGGER, PIN_ECHO_L, false);
 
+const byte arduAddress = I2C_ADDRESS; // I2C address of the Arduino board
+bool dataReady = false;
+int dataReceived = 0;
+
+void handleI2CInterrupt()
+{
+  printf("I2C interrupt triggered\n");
+  dataReady = true;
+}
+
+void processI2CData()
+{
+  if (dataReady)
+  {
+    // Read data from I2C device
+    dataReceived = Wire.read(); // Read the data from the I2C bus
+    Serial.print("Data received: ");
+    Serial.println(dataReceived);
+
+    dataReady = false;
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
+  Wire.begin(arduAddress); // Initialize I2C with specified SDA and SCL pins
+
+  attachInterrupt(digitalPinToInterrupt(I2C_INTERRUPT_PIN), handleI2CInterrupt, FALLING);
 }
 
 void loop()
@@ -63,6 +92,8 @@ void loop()
   float distFront = sensorFront.getDistance();
   float distRight = sensorRight.getDistance();
   float distLeft = sensorLeft.getDistance();
+
+  processI2CData(); // Process I2C data if available
 
   if ((distRight > 0 && distRight <= EMERGENCY_STOP_DISTANCE) || (distLeft > 0 && distLeft <= EMERGENCY_STOP_DISTANCE) || (distFront > 0 && distFront <= 10))
   {

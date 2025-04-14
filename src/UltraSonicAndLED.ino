232171fe110687b8421727a07c102d61dab60d5f
Dapringer <allerlei-03@web.de>
2025-04-14 10:48:14 +0200

    refactor sensor handling and emergency stop logic for improved wall detection and response

--- src/UltraSonicAndLED.ino
+++ src/UltraSonicAndLED.ino
@@ -5,7 +5,7 @@
 #include "class/Motor.h"
 #include "class/Sensor.h"
 
-#define SENSOR_TRGGER 21
+#define SENSOR_TRGGER 4
 #define PIN_TRIGGER_F 12
 #define PIN_ECHO_F 2
 #define PIN_TRIGGER_R 21
@@ -22,8 +22,8 @@
 #define In4 12
 #define EnB 13
 
-#define MAX_SPEED 150
-#define MIN_SPEED 75
+#define MAX_SPEED 100
+#define MIN_SPEED 50
 #define EMERGENCY_STOP_DISTANCE 5
 
 using namespace std;
@@ -48,31 +48,66 @@ void setup()
 
 void loop()
 {
-  // sensorFront.printStatus();
-  // sensorFront.isWallApproaching();
-  // sensorRight.printStatus();
-  // sensorRight.isWallApproaching();
-  // sensorBack.printStatus();
-  // sensorBack.isWallApproaching();
-  // sensorLeft.printStatus();
-  // sensorLeft.isWallApproaching();
 
   bool wallFront = sensorFront.isWallApproaching();
   bool wallRight = sensorRight.isWallApproaching();
+  bool wallBack = sensorBack.isWallApproaching();
   bool wallLeft = sensorLeft.isWallApproaching();
+
   float distFront = sensorFront.getDistance();
   float distRight = sensorRight.getDistance();
+  float distBack = sensorBack.getDistance();
   float distLeft = sensorLeft.getDistance();
 
-  if ((distRight > 0 && distRight <= EMERGENCY_STOP_DISTANCE) || (distLeft > 0 && distLeft <= EMERGENCY_STOP_DISTANCE) || (distFront > 0 && distFront <= 10))
+  if (wallFront)
   {
-    motorLeft.emergencyStop();
-    motorRight.emergencyStop();
-    Serial.println("Emergency stop activated!");
-    return;
-  }
+    // Approach the wall slowly until closer than 15 cm
+    if (distFront > MIN_DIST)
+    {
+      motorLeft.drive(MIN_SPEED, 1);
+      motorRight.drive(MIN_SPEED, 1);
+    }
+    else
+    {
+      // Stop when closer than 15 cm
+      motorLeft.emergencyStop();
+      motorRight.emergencyStop();
+      delay(1000);
 
-  if (wallRight && wallLeft)
+      // Check if there's space to back up
+      if (!wallBack)
+      {
+        // Back up
+        motorLeft.drive(MAX_SPEED, 2);
+        motorRight.drive(MAX_SPEED, 2);
+        delay(2000);
+
+        // Turn in a random direction
+        if (random(0, 2) == 0)
+        {
+          // Turn right
+          motorLeft.drive(MAX_SPEED, 1);
+          motorRight.drive(MIN_SPEED, 1);
+        }
+        else
+        {
+          // Turn left
+          motorLeft.drive(MIN_SPEED, 1);
+          motorRight.drive(MAX_SPEED, 1);
+        }
+        delay(1000);
+
+        // Stop after turning
+        motorLeft.drive(0, 0);
+        motorRight.drive(0, 0);
+      }
+      else
+      {
+        Serial.println("Cannot back up, obstacle detected!");
+      }
+    }
+  }
+  else if (wallRight && wallLeft)
   {
     // Both sensors detect a wall – avoid the closer one
     if (distRight < distLeft)
@@ -100,11 +135,18 @@ void loop()
     motorRight.drive(MIN_SPEED, 1);
     motorLeft.drive(MAX_SPEED, 1);
   }
-  else
+  else if (distFront > 0 && distFront > EMERGENCY_STOP_DISTANCE && distRight > EMERGENCY_STOP_DISTANCE && distLeft > EMERGENCY_STOP_DISTANCE)
   {
     // No wall approaching → go straight
     motorRight.drive(MAX_SPEED, 1);
     motorLeft.drive(MAX_SPEED, 1);
+  }
+  else
+  {
+    // Emergency stop if any condition is unsafe
+    motorLeft.emergencyStop();
+    motorRight.emergencyStop();
+    Serial.println("Emergency stop activated!");
   }
 
   Serial.println("------------------");

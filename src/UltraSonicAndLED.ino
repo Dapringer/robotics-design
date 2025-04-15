62dd4f63420f6d70d617eb80561f842c3f057901
Dapringer <allerlei-03@web.de>
2025-04-15 15:48:37 +0200

    refactor: clean up sensor definitions and streamline wall detection logic

--- src/UltraSonicAndLED.ino
+++ src/UltraSonicAndLED.ino
@@ -5,7 +5,6 @@
 #include "class/Motor.h"
 #include "class/Sensor.h"
 
-#define SENSOR_TRGGER 4
 #define PIN_TRIGGER_F 22
 #define PIN_ECHO_F 2
 #define PIN_TRIGGER_R 24
@@ -25,12 +24,6 @@
 #define I2C_SDA 20
 #define I2C_SCL 21
 #define I2C_ADDRESS 0x08
-#define I2C_INTERRUPT_PIN 18
-
-#define I2C_SDA 20
-#define I2C_SCL 21
-#define I2C_ADDRESS 0x08
-#define I2C_INTERRUPT_PIN 18
 
 #define MAX_SPEED 150
 #define MIN_SPEED 100
@@ -40,12 +33,6 @@
 #define EMERGENCY_STOP_DISTANCE_SIDES 10
 #define EMERGENCY_STOP_DISTANCE_FRONT 20
 
-
-// const int SENSOR_MAX_RANGE = 300; // in cm
-const int MIN_DIST = 15;
-unsigned long duration;
-unsigned int distance;
-
 Motor motorRight(1, In1, In2, EnA);
 Motor motorLeft(2, In3, In4, EnB);
 
@@ -54,11 +41,21 @@ Sensor sensorRight(2, PIN_TRIGGER_R, PIN_ECHO_R, true);
 Sensor sensorBack(3, PIN_TRIGGER_B, PIN_ECHO_B, true);
 Sensor sensorLeft(4, PIN_TRIGGER_L, PIN_ECHO_L, true);
 
+bool wallFront = false;
+bool wallRight = false;
+bool wallLeft = false;
+bool wallBack = false;
+
+float distFront = 0;
+float distRight = 0;
+float distLeft = 0;
+float distback = 0;
+
 const byte arduAddress = I2C_ADDRESS; // I2C address of the Arduino board
 int dataReceived = 0;
 volatile bool newDataAvailable = false;
-bool stopOverwrite = false;
 
+bool stopOverwrite = false;
 bool humanInFront = false;
 
 void receiveEvent(int howMany)
@@ -134,33 +131,20 @@ void setup()
 
 void loop()
 {
+  // Process I2C data if available
+  processI2CData();
 
-  processI2CData(); // Process I2C data if available
-
+  // If stopOverwrite is true skip wall detection and listen for new I2C commands
   if (stopOverwrite)
   {
-    // If stopOverwrite is true, stop the robot and skip wall detection
     return;
   }
 
-  bool wallFront = sensorFront.isWallApproaching();
-  bool wallRight = sensorRight.isWallApproaching();
-  bool wallLeft = sensorLeft.isWallApproaching();
-  bool wallBack = sensorBack.isWallApproaching();
-
-  float distFront = sensorFront.getDistance();
-  float distRight = sensorRight.getDistance();
-  float distLeft = sensorLeft.getDistance();
-  float distback = sensorBack.getDistance();
+  readSensors();
 
   if (wallFront || (distFront < EMERGENCY_STOP_DISTANCE_FRONT && distFront > 0))
   {
     // Wall detected in front → back up
-    Serial.println("Wall detected in front! Backing up...");
-    backUpFromWall();
-  }
-  else if (humanInFront && wallFront)
-  {
     Serial.println("Wall detected in front! Backing up...");
     backUpFromWall();
   }
@@ -171,26 +155,26 @@ void loop()
     {
       // Right is closer → steer left
       Serial.println("Steering left to avoid wall on the right... Right closer than left");
-      turnAwayFromWall(3);
+      avoidWall(3);
     }
     else
     {
       // Left is closer → steer right
       Serial.println("Steering right to avoid wall on the left... Left closer than right");
-      turnAwayFromWall(4);
+      avoidWall(4);
     }
   }
   else if (wallRight)
   {
     // Only right detects wall → steer left
     Serial.println("Steering left to avoid wall on the right...");
-    turnAwayFromWall(3);
+    avoidWall(3);
   }
   else if (wallLeft)
   {
     // Only left detects wall → steer right
     Serial.println("Steering right to avoid wall on the left...");
-    turnAwayFromWall(4);
+    avoidWall(4);
   }
   else if (distFront > EMERGENCY_STOP_DISTANCE_FRONT &&
            (distRight == 0 || distRight > EMERGENCY_STOP_DISTANCE_SIDES) &&
@@ -210,15 +194,31 @@ void loop()
   delay(50);
 }
 
+// Reading sensors and updating the wall detection status
+void readSensors()
+{
+  wallFront = sensorFront.isWallApproaching();
+  wallRight = sensorRight.isWallApproaching();
+  wallLeft = sensorLeft.isWallApproaching();
+  wallBack = sensorBack.isWallApproaching();
+
+  distFront = sensorFront.getDistance();
+  distRight = sensorRight.getDistance();
+  distLeft = sensorLeft.getDistance();
+  distback = sensorBack.getDistance();
+}
+
 void driveRobot(int direction)
 {
   if (direction == 3)
   {
+    // Turn Left
     motorLeft.drive(0, 2);
     motorRight.drive(TURNING_SPEED, 1);
   }
   else if (direction == 4)
   {
+    // Turn Right
     motorLeft.drive(TURNING_SPEED, 1);
     motorRight.drive(0, 2);
   }
@@ -230,19 +230,26 @@ void driveRobot(int direction)
   }
 }
 
-void turnAwayFromWall(int direction)
+// Avoid wall by turning left or right for a short time
+void avoidWall(int direction)
 {
   unsigned long turnStartTime = millis();
 
-  // Turn left or right randomly for 2 seconds
   for (; millis() - turnStartTime < TURNING_TIME;)
   {
     driveRobot(direction);
   }
 
-  driveRobot(0); // Stop after turning
+  driveRobot(0);
 }
 
+// Back up from wall if detected in front. Sequence is as follows:
+// 1. If Human in Front, do nothing
+// 2. Stop motors for 1 second
+// 3. If no wall in Back, back up for 3 seconds
+// 4. If wall detected, emergency Stop
+// 5. Otherwise turn randomly for 2 seconds
+// 6. Stop motors
 void backUpFromWall()
 {
   if (humanInFront)
@@ -252,7 +259,6 @@ void backUpFromWall()
   }
 
   float distFront = sensorFront.getDistance();
-
 
   motorLeft.emergencyStop();
   motorRight.emergencyStop();
@@ -293,7 +299,7 @@ void backUpFromWall()
         delay(50); // Small delay to avoid busy-waiting
       }
 
-      driveRobot(0); // Stop after turning
+      driveRobot(0);
     }
   }
   else
